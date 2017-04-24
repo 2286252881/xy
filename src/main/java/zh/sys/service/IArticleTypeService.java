debaaddafa610da70c8639383ef2824dfb8314a8
2286252881 <2286252881@qq.com>
2017-04-24 09:45:40 +0800

    查询文章详情业务接口

--- src/main/java/zh/sys/service/IArticleTypeService.java
+++ src/main/java/zh/sys/service/IArticleTypeService.java
@@ -2,13 +2,19 @@ package zh.sys.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import zh.base.entity.TArticle;
 import zh.base.entity.TArticletype;
 
 public interface IArticleTypeService {
 	public List<TArticletype> getArticleType(TArticletype tArticletype);
+
 	// 查询v1会员最新的10条数据
 	// 查询v2会员最新的10条数据
 	// 查询v3会员最新的10条数据
 	public List<TArticle> getArticles(TArticle article);
+
+	// 根据文章id获取文章详细信息
+	public TArticle getArticle(TArticle article);
 }
