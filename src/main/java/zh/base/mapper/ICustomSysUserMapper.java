4a20bc28c20ccd9599c7cba1fa635c4a2d0b3570
2286252881 <2286252881@qq.com>
2017-04-24 09:47:08 +0800

    查询文章详情接口

--- src/main/java/zh/base/mapper/ICustomSysUserMapper.java
+++ src/main/java/zh/base/mapper/ICustomSysUserMapper.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import zh.base.entity.TArticle;
 import zh.base.entity.TRole;
 import zh.base.entity.TUser;
 
 public interface ICustomSysUserMapper {
 	// 根据登陆用户获取登陆用户所有角色
 	public List<TRole> getRoles(@Param("user")TUser user);
+	
+	//根据文章id获取文章详细信息
+	public TArticle getArticle(@Param("article") TArticle article);
 }
